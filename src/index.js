c1a01df2b2fb681399f657e17d9dcfbe17cddfec
Moh Aadil <aadilrehman.development@gmail.com>
2025-04-06 01:58:50 +0530

    structured app and connected to DB

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const mongoDBconnect = require("./config/database");
+require("dotenv").config();
 
 const app = express();
 const PORT = 3000;
@@ -7,6 +9,13 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => {
-	console.log(`Server listening on Port ${PORT}`);
-});
+mongoDBconnect()
+	.then(() => {
+		console.log("Database connection establised.");
+		app.listen(PORT, () => {
+			console.log(`Server listening on Port ${PORT}`);
+		});
+	})
+	.catch(() => {
+		console.log("Database cannot be connected");
+	});
