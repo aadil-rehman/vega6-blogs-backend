d48e60e256e9fa34c2167fcf87df07f0b193cda0
Moh Aadil <aadilrehman.development@gmail.com>
2025-04-07 01:51:58 +0530

    add: logout api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -55,4 +55,16 @@ authRouter.post("/login", async (req, res) => {
 	}
 });
 
+authRouter.post("/logout", async (req, res) => {
+	try {
+		res
+			.cookie("token", null, {
+				expires: new Date(Date.now()),
+			})
+			.send("Logout successfull!");
+	} catch (err) {
+		res.status(400).json({ error: err.message });
+	}
+});
+
 module.exports = authRouter;
